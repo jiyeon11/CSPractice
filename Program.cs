568edd5194eb9b6cb69a2f7ff9a1bff8dd49b70e
jiyeon11 <leejiyeon415@gmail.com>
2024-07-02 14:49:06 +0900

    #5 22-9. 값 복사와 참조 복사

--- Program.cs
+++ Program.cs
@@ -76,6 +76,15 @@ namespace CSPractice
 
     internal class Program
     {
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test t)
+        {
+            t.value = 20;
+        }
+
         class ThirdClass
         {
 
@@ -284,6 +293,11 @@ namespace CSPractice
             b.Height = 100;
             b.Height = -100;
 
+            // #5 22-9. 값 복사와 참조 복사
+            Test test = new Test();
+            test.value = 100;
+            Change(test);
+            Console.WriteLine(test.value); //  20 
         }
     }
 }
